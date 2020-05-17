e19a3d714d3a57526067c035f546f644cbeb83d9
Bo Hwang <bo1.hwang@sea.samsung.com>
2020-05-17 18:38:37 -0500

    adds a python script

---
+++ Coursera_Capstone.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook will be mainly used for the capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project Course!")
+
